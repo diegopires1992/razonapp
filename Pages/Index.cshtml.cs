b37912d9d6a39af846c4385f070b1f85388ab015
Diego Pires <diego__pires@hotmail.com>
2024-01-04 09:16:53 -0300

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyRazonApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+        public void OnPost()
+        {
+        }
+    }
+}
